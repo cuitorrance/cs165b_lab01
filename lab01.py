41173c919e7add42f573d0032ac02ec93fe5f255
Torrance Cui <torrancecui@cs.ucsb.edu>
2020-01-23 21:21:30 -0800

    yuhhh

--- lab01.py
+++ lab01.py
@@ -48,3 +48,26 @@ def predict(row, coefficients):
 	for i in range(len(row)-1):
 		yhat += coefficients[i + 1] * row[i]
 	return yhat
+
+# Estimate linear regression coefficients using stochastic gradient descent
+def coefficients_sgd(train, l_rate, n_epoch):
+	coef = [0.0 for i in range(len(train[0]))]
+	for epoch in range(n_epoch):
+		for row in train:
+			yhat = predict(row, coef)
+			error = yhat - row[-1]
+			coef[0] = coef[0] - l_rate * error
+			for i in range(len(row)-1):
+				coef[i + 1] = coef[i + 1] - l_rate * error * row[i]
+			# print(l_rate, n_epoch, error)
+	return coef
+ 
+# Linear Regression Algorithm With Stochastic Gradient Descent
+def linear_regression_sgd(train, test, l_rate, n_epoch):
+	predictions = list()
+	coef = coefficients_sgd(train, l_rate, n_epoch)
+	for row in test:
+		yhat = predict(row, coef)
+		predictions.append(yhat)
+	return(predictions)
+
