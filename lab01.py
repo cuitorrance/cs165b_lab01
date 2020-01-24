a03d96a5662ea5d14aa70e8f1c69bab65e086d87
Torrance Cui <torrancecui@cs.ucsb.edu>
2020-01-23 21:20:12 -0800

    yuh

--- lab01.py
+++ lab01.py
@@ -1,25 +1,50 @@
 import csv
 import numpy as np
+from random import seed
+from random import randrange
+from csv import reader
+from math import sqrt
 
-filename = 'Admission_Predict.csv'
-
-
-# reading csv file and input into array
-def readcsv(filename):
-    ifile = open(filename, "r")
-    reader = csv.reader(ifile, delimiter=";")
-
-    rownum = 0
-    a = []
-
-    for row in reader:
-        a.append(row)
-        rownum += 1
-
-    ifile.close()
-    return a
+# Load a CSV file
+def load_csv(filename):
+	dataset = list()
+	with open(filename, 'r') as file:
+		csv_reader = reader(file)
+		for row in csv_reader:
+			if not row:
+				continue
+			dataset.append(row)
+	return dataset
 
 
-x = readcsv(filename)
+#find min and max
+def dataset_minmax(dataset):
+	minmax = list()
+	for i in range(len(dataset[0])):
+		col_values = [row[i] for row in dataset]
+		value_min = min(col_values)
+		value_max = max(col_values)
+		minmax.append([value_min, value_max])
+	return minmax
 
-print("hello world")
+# # Rescale dataset columns to the range 0-1
+# def normalize_dataset(dataset, minmax):
+# 	for row in dataset:
+# 		for i in range(len(row)):
+# 			row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
+
+# Calculate root mean squared error
+def rmse_metric(actual, predicted):
+	sum_error = 0.0
+	for i in range(len(actual)):
+		prediction_error = predicted[i] - actual[i]
+		sum_error += (prediction_error ** 2)
+	mean_error = sum_error / float(len(actual))
+	return sqrt(mean_error)
+
+# Make a prediction with coefficients
+def predict(row, coefficients):
+	yhat = coefficients[0]
+	for i in range(len(row)-1):
+		yhat += coefficients[i + 1] * row[i]
+	return yhat
